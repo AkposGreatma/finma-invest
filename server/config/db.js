0aa927dac9ddcba8fd814a4bcc9fa80ecb2ef65b
AkposGreatma <oghenemarogreat77@gmail.com>
2026-05-12 20:33:59 +0100

    configured vercel json file

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,21 @@
 const mysql = require("mysql2");
+
 require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: Number(process.env.PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err.message);
+    console.error("Database connection failed:", err);
   } else {
     console.log("MySQL connected");
   }
